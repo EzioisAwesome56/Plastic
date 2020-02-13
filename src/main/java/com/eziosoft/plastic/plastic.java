c492b4f62fc9372bc42344c51a06153be77d71fb
Jon <jedijon567@gmail.com>
2020-02-12 21:38:06 -0500

    world gen didnt work; add raw plastic instead

--- src/main/java/com/eziosoft/plastic/plastic.java
+++ src/main/java/com/eziosoft/plastic/plastic.java
@@ -1,9 +1,13 @@
 package com.eziosoft.plastic;
 
+import com.mojang.datafixers.Dynamic;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biomes.v1.OverworldBiomes;
 import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.tools.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -14,6 +18,17 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.carver.CarverConfig;
+import net.minecraft.world.gen.carver.ConfiguredCarver;
+import net.minecraft.world.gen.chunk.OverworldChunkGeneratorConfig;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.*;
+import sun.jvm.hotspot.gc.shared.Generation;
+import sun.security.krb5.Config;
 
 public class plastic implements ModInitializer {
 
@@ -22,6 +37,7 @@ public class plastic implements ModInitializer {
 	public static final Block plastic_ore = new FuckBlock(FabricBlockSettings.of(Material.STONE).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 2).hardness(2).resistance(0.2F).build());
 	public static final Item plastic_dust = new Item(new Item.Settings().group(ItemGroup.MISC));
 	public static final Item plastic_mix = new Item(new Item.Settings().maxCount(1).group(ItemGroup.MISC));
+	public static final Item raw_plastic = new Item(new Item.Settings().group(ItemGroup.MISC));
 	public static final Item apple_plastic = new Item(new Item.Settings().group(ItemGroup.MISC).maxCount(1).food(new FoodComponent.Builder().hunger(9).saturationModifier(6f).statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 10), 0.2F).build()));
 
 	@Override
@@ -38,6 +54,8 @@ public class plastic implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("plastic", "plastic_dust"), plastic_dust);
 		Registry.register(Registry.ITEM, new Identifier("plastic", "plastic_mix"), plastic_mix);
 		Registry.register(Registry.ITEM, new Identifier("plastic", "apple_plastic"), apple_plastic);
+		Registry.register(Registry.ITEM, new Identifier("plastic", "raw_plastic"), raw_plastic);
+
 	}
 
 }
