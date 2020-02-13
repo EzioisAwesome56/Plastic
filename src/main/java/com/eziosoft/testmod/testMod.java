0730892aa72e2bac8c4a674c88a8f707809b5e92
Jon <jedijon567@gmail.com>
2020-02-12 19:06:13 -0500

    added plastic

--- src/main/java/com/eziosoft/testmod/testMod.java
+++ src/main/java/com/eziosoft/testmod/testMod.java
@@ -13,8 +13,9 @@ import net.minecraft.util.registry.Registry;
 
 public class testMod implements ModInitializer {
 
-	//public static final Block plastic_ore = new Block(FabricBlockSettings.of(Material.METAL).build());
-public static final Block plastic_ore = new FuckBlock(FabricBlockSettings.of(Material.STONE).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 2).hardness(2).resistance(0.2F).build());
+
+	public static final Item plastic = new Item(new Item.Settings().group(ItemGroup.MISC));
+	public static final Block plastic_ore = new FuckBlock(FabricBlockSettings.of(Material.STONE).breakByHand(false).breakByTool(FabricToolTags.PICKAXES, 2).hardness(2).resistance(0.2F).build());
 	@Override
 	public void onInitialize() {
 
@@ -25,6 +26,7 @@ public static final Block plastic_ore = new FuckBlock(FabricBlockSettings.of(Mat
 		System.out.println("Loading blocks");
 		Registry.register(Registry.BLOCK, new Identifier("testmod", "plastic_ore"), plastic_ore);
 		Registry.register(Registry.ITEM, new Identifier("testmod", "plastic_ore"), new BlockItem(plastic_ore, new Item.Settings().group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, new Identifier("testmod", "plastic"), plastic);
 	}
 
 }
