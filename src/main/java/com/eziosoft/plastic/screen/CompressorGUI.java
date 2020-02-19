38a90c2a89dfdb004354a0ce9566d9425b815337
Jon <jedijon567@gmail.com>
2020-02-19 00:29:31 -0500

    more crafting redos

--- src/main/java/com/eziosoft/plastic/screen/CompressorGUI.java
+++ src/main/java/com/eziosoft/plastic/screen/CompressorGUI.java
@@ -4,6 +4,7 @@ import com.eziosoft.plastic.stuff.PlasticCombinerHandler;
 import com.sun.javafx.image.IntPixelGetter;
 import io.github.cottonmc.cotton.gui.CottonCraftingController;
 import io.github.cottonmc.cotton.gui.widget.*;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.container.BlockContext;
 import net.minecraft.container.SlotActionType;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.crash.CrashReport;
 import sun.jvm.hotspot.utilities.soql.Callable;
 
 import java.lang.reflect.WildcardType;
@@ -21,10 +23,10 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class CompressorGUI extends CottonCraftingController {
 
-    private static WLabel status = new WLabel(new TranslatableText("gui.plastic.wait").asString());
-
     public CompressorGUI(int syncId, PlayerInventory playerInventory, BlockContext context) {
         super(RecipeType.SMELTING, syncId, playerInventory, getBlockInventory(context), getBlockPropertyDelegate(context));
+
+        WLabel status = new WLabel(new TranslatableText("gui.plastic.wait").asString());
 
         WGridPanel root = new WGridPanel();
         setRootPanel(root);
@@ -32,50 +34,20 @@ public class CompressorGUI extends CottonCraftingController {
         WLabel label = new WLabel(new TranslatableText("block.plastic.plastic_compressor").asString());
         blockInventory = getBlockInventory(context);
 
-        // test
-        if (blockInventory.countInInv(Items.STICK) == 2 && blockInventory.getInvStack(3).isEmpty()) {
-            // clear all the slots
-            blockInventory.clear();
-            // give player something
-            blockInventory.setInvStack(3, new ItemStack(Items.ACACIA_DOOR, 7));
+        // handle crafting
+        if (blockInventory.getInvStack(3).isEmpty()) {
+            if (blockInventory.countInInv(Items.STICK) == 2) {
+                // clear all the slots
+                blockInventory.clear();
+                // give player something
+                blockInventory.setInvStack(3, new ItemStack(Items.ACACIA_DOOR, 7));
+                status = new WLabel(new TranslatableText("gui.plastic.compressed").asString());
+            }
+        } else {
+            status = new WLabel(new TranslatableText("gui.plastic.full").asString());
         }
 
 
-
-
-
-        WButton craft = new WButton(new TranslatableText("gui.plastic.compress")).setOnClick(new Runnable() {
-            @Override
-            public void run() {
-                // clear the label right at the start
-                root.remove(status);
-                // check if something is in the output slot
-                if (!blockInventory.getInvStack(3).isEmpty()){
-                    status = new WLabel(new TranslatableText("gui.plastic.full").asString());
-                    root.add(status, 0, 2);
-                    return;
-                }
-                // check if all the input slots are empty
-                if (blockInventory.getInvStack(0).isEmpty() && blockInventory.getInvStack(1).isEmpty() && blockInventory.getInvStack(2).isEmpty()){
-                    status = new WLabel(new TranslatableText("gui.plastic.empty").asString());
-                    root.add(status, 0, 2);
-                    return;
-                }
-                // from this point forwards, check all crafting recipes
-                if (blockInventory.countInInv(Items.STICK) == 2){
-                    // clear all the slots
-                    blockInventory.clear();
-                    // give player something
-                    blockInventory.setInvStack(3, new ItemStack(Items.ACACIA_DOOR, 7));
-                    // update status
-                    status = new WLabel(new TranslatableText("gui.plastic.compressed").asString());
-                    root.add(status, 0, 2);
-
-                    return;
-                }
-                System.out.println("Do more stuff here");
-            }
-        });
         root.add(label, 0, 0);
 
         for (int x = 0; x < blockInventory.getInvSize() - 1; x++){
@@ -84,11 +56,10 @@ public class CompressorGUI extends CottonCraftingController {
         root.add(WItemSlot.of(blockInventory, 3), 5, 1);
 
         root.add(status, 0, 2);
-        root.add(craft, 0, 3, 4, 1);
         playerInventory.updateItems();
 
 
-        root.add(this.createPlayerInventoryPanel(), 0, 5);
+        root.add(this.createPlayerInventoryPanel(), 0, 4);
 
         root.validate(this);
     }
